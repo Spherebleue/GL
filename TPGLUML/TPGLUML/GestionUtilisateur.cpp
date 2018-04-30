b79f172a4ed446efcb4aeceb8ca0f2d17468f5d7
gooluke <36793393+gooluke@users.noreply.github.com>
2018-04-30 15:47:13 +0200

    Update GestionUtilisateur.cpp

--- TPGLUML/TPGLUML/GestionUtilisateur.cpp
+++ TPGLUML/TPGLUML/GestionUtilisateur.cpp
@@ -1,11 +1,17 @@
+
 #include "GestionUtilisateur.h"
-
-
 
 GestionUtilisateur::GestionUtilisateur()
 {
 }
 
+void CreerCompte(string nom, string prenom, string motpasse, string adresse)
+{
+}
+
+void AffichageListeDocteurs(String nomFichier)
+{
+}
 
 GestionUtilisateur::~GestionUtilisateur()
 {
