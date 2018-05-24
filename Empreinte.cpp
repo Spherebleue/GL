5a283abf1779bbad5d91c8ab55eb36575d7e3af1
WingOfRaven <38857418+WingOfRaven@users.noreply.github.com>
2018-05-24 08:49:52 +0200

    maj algo init empreinte

---
+++ Empreinte.cpp
@@ -0,0 +1,142 @@
+/*************************************************************************
+                           Empreinte  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <Empreinte> (fichier Empreinte.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+
+//------------------------------------------------------ Include personnel
+#include "Empreinte.h"
+#include <sstream>
+
+using namespace std;
+//------------------------------------------------------------- Constantes
+//#define MAP ;
+
+//----------------------------------------------------------------- PUBLIC
+string Empreinte::fichierFormat = "F1_ok.txt";
+vector< pair<string, string> > Empreinte::format;
+
+void Empreinte::initialiserFormat()
+{
+	
+    ifstream entreeFichier;
+    entreeFichier.open(fichierFormat);
+
+    assert(entreeFichier);                        //verifie que le fichier est bien trouve
+
+    string line;
+    getline(entreeFichier, line);
+
+    assert(line.compare("AttributeName;AttributeType")==0);    //verifie que l'entete est bien la bonne
+    getline(entreeFichier, line);
+    assert(line.compare("NoID;ID")==0);
+
+    while(entreeFichier.good())
+    {
+		getline(entreeFichier, line);
+        stringstream ss;
+        ss.str(line);
+        string nomAttribut;
+        getline(ss, nomAttribut,';');
+
+        assert(nomAttribut!=line);                  //veririe que la ligne est bien formee
+
+        string typeAttribut;
+        getline(ss, typeAttribut);
+
+        if(typeAttribut.compare("string")==0)       //verifie que le type est bien le bon
+        {
+            typeAttribut="Categoriel";
+        }
+        else if (typeAttribut.compare("double")==0)
+        {
+            typeAttribut="Numerique";
+        }
+        else
+        {
+            cout<<"Erreur : l'un des attributs n'est ni numerique, ni categoriel"<<endl;
+            abort();
+        }
+
+        pair<string,string> nouvellePaire=make_pair(nomAttribut,typeAttribut);
+        format.push_back(nouvellePaire);
+
+        
+    }
+
+}
+
+//----------------------------------------------------- Méthodes publiques
+/*void Empreinte::ajouterAttribut(Attribut * attribut)
+{
+    Attribut a = new Attribut(*attribut);
+	attributs.push_back(a);
+}*/
+
+
+//-------------------------------------------- Constructeurs - destructeur
+Empreinte::Empreinte ( const Empreinte & unEmpreinte )
+// Algorithme :
+// Un constructeur de copie.
+	{
+		#ifdef MAP
+			cout << "Appel au constructeur de copie de <Empreinte>" << endl;
+		#endif
+
+	} //----- Fin de Empreinte (constructeur de copie)
+
+
+Empreinte::Empreinte ()
+// Algorithme :
+// Un constructeur par défaut.
+	{
+		#ifdef MAP
+			cout << "Appel au constructeur de <Empreinte>" << endl;
+		#endif
+       /* if( this->format.empty())
+        {
+            initialiserFormat();
+        }*/
+	} //----- Fin de Empreinte
+
+
+Empreinte::Empreinte (std::vector <Attribut> nouveauxAttributs)
+// Algorithme :
+// Un constructeur par défaut.
+	{
+		#ifdef MAP
+			cout << "Appel au constructeur de <Empreinte>" << endl;
+		#endif
+       /* if( this->format.empty())
+        {
+            initialiserFormat();
+        }
+		this->ID =ID;
+
+		assert(this->format.size()==nouveauxAttributs.size());
+*/
+        this->attributs=nouveauxAttributs;
+
+	} //----- Fin de Empreinte
+
+
+
+Empreinte::~Empreinte ( )
+// Algorithme :
+//
+	{
+		#ifdef MAP
+		    cout << "Appel au destructeur de <Empreinte>" << endl;
+		#endif
+	} //----- Fin de ~Empreinte
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
---
+++ Empreinte.h
@@ -0,0 +1,59 @@
+#if ! defined ( Empreinte_H )
+#define Empreinte_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+#include "Attribut.h"
+using namespace std;
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <utility>
+#include <vector>
+#include <assert.h>
+
+class Empreinte
+{
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------Fonction amies
+public:
+
+    static string fichierFormat;
+	static vector< pair<string, string> > format;
+    //void ajouterAttribut(Attribut *);
+
+    void static initialiserFormat();
+
+//-------------------------------------------- Constructeurs - destructeur
+    Empreinte ( const Empreinte & uneEmpreinte );
+    // Mode d'emploi :
+    // Fait une copie en profondeur de l'objet donne en parametre, et
+    // l ajoute au Empreinte.
+
+    Empreinte ();
+    // Mode d'emploi :
+    // Un constructeur par défaut, se contentant de creer la premiere cellule
+
+	Empreinte(std::vector <Attribut> nouveauxAttributs);
+
+
+
+    virtual ~Empreinte ( );
+    // Mode d'emploi :
+    // Destructeur de Empreinte
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+	string ID;
+	vector <Attribut> attributs;
+
+
+};
+
+
+
+#endif
